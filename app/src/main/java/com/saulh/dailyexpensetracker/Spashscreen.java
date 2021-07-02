684e257db7b0e75a243c8f0dc83fd527695a6758
saulh <saulhernandez217@gmail.com>
2021-07-01 22:56:23 -0700

    Splash screen -> login

--- app/src/main/java/com/saulh/dailyexpensetracker/Spashscreen.java
+++ app/src/main/java/com/saulh/dailyexpensetracker/Spashscreen.java
@@ -19,7 +19,7 @@ public class Spashscreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
                 startActivity(intent);
                 finish(); //kill this activity
             }
