8867e575deb9f212fc3842248af6f30c5ee0fc49
saulh <saulhernandez217@gmail.com>
2021-07-01 19:40:38 -0700

    Completed rough user login and registration

--- app/src/main/java/com/saulh/dailyexpensetracker/LoginActivity.java
+++ app/src/main/java/com/saulh/dailyexpensetracker/LoginActivity.java
@@ -2,13 +2,72 @@ package com.saulh.dailyexpensetracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.saulh.dailyexpensetracker.daos.UserDao;
+import com.saulh.dailyexpensetracker.entities.User;
+
+import java.io.Serializable;
 
 public class LoginActivity extends AppCompatActivity {
+    Button mBtnLogin, mBtnRegister;
+    EditText mEditTextUsername, mEditTextPassword;
+
+    Context context;
+
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        context = getApplicationContext();
+
+        mBtnLogin = findViewById(R.id.btnLogin);
+        mBtnRegister = findViewById(R.id.btnToRegistration);
+
+        mEditTextUsername = findViewById(R.id.editTextLoginUsername);
+        mEditTextPassword = findViewById(R.id.editTextLoginPassword);
+
+        mBtnLogin.setOnClickListener(v -> {
+            user = loginWithCredentials(mEditTextUsername.getText().toString(),
+                    mEditTextPassword.getText().toString());
+            if (user != null) {
+                mEditTextPassword.setText("");
+                Intent intent = new Intent(context, MainActivity.class);
+                intent.putExtra("username", user.username);
+                startActivity(intent);
+            }
+        });
+
+        mBtnRegister.setOnClickListener(v -> {
+            mEditTextPassword.setText("");
+            Intent intent = new Intent(context, RegisterActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    private User loginWithCredentials(String username, String password) {
+        AppDatabase db = AppDatabase.getDBInstance(context);
+        UserDao userDao = db.userDao();
+        User user = userDao.getUserByUsername(username);
+        if (user != null) {
+            if (password.equals(user.password)) {
+                return user;
+            } else {
+                return null;
+            }
+        } else {
+            Toast.makeText(context,
+                    "Username and/or password are incorrect",
+                    Toast.LENGTH_LONG).show();
+            return null;
+        }
     }
 }
--- app/src/main/java/com/saulh/dailyexpensetracker/RegisterActivity.java
+++ app/src/main/java/com/saulh/dailyexpensetracker/RegisterActivity.java
@@ -2,13 +2,111 @@ package com.saulh.dailyexpensetracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.saulh.dailyexpensetracker.daos.UserDao;
+import com.saulh.dailyexpensetracker.entities.User;
 
 public class RegisterActivity extends AppCompatActivity {
+    private AppDatabase db;
+    private Button mBtnRegister;
+    private Context context;
+    private EditText mEditTextUsername,
+                     mEditTextEmail,
+                     mEditTextPassword,
+                     mEditTextReenterPass;
+    private UserDao userDao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        context = getApplicationContext();
+        db = AppDatabase.getDBInstance(context);
+        userDao = db.userDao();
+
+        mBtnRegister = findViewById(R.id.btnRegister);
+
+        mEditTextUsername = findViewById(R.id.editTextUsername);
+        mEditTextEmail = findViewById(R.id.editTextEmail);
+        mEditTextPassword = findViewById(R.id.editTextRegPassword);
+        mEditTextReenterPass = findViewById(R.id.editTextReenterPassword);
+
+
+
+        mBtnRegister.setOnClickListener(v -> {
+            if (inputIsValid()) {
+                registerUser();
+            } else {
+                Toast.makeText(context, "Please check errors", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void registerUser() {
+        User user = new User();
+        String usernameInput = mEditTextUsername.getText().toString();
+        String emailInput = mEditTextEmail.getText().toString();
+        String passwordInput = mEditTextPassword.getText().toString();
+
+        user.username = usernameInput;
+        user.email = emailInput;
+        user.password = passwordInput;
+
+        db.userDao().registerUser(user);
+        finish();
+    }
+
+    private boolean inputIsValid() {
+        boolean isValid = true;
+        String usernameInput = mEditTextUsername.getText().toString();
+        String emailInput = mEditTextEmail.getText().toString();
+        String passwordInput = mEditTextPassword.getText().toString();
+        String passwordReenterInput = mEditTextReenterPass.getText().toString();
+
+        if (usernameInput.isEmpty()) {
+            mEditTextUsername.setError("Username cannot be empty");
+            isValid = false;
+        } else if (db.userDao().getUserByUsername(usernameInput) != null) {
+            mEditTextUsername.setError("Username is already taken");
+            isValid = false;
+        } else {
+            mEditTextUsername.setError(null);
+        }
+
+        if (emailInput.isEmpty()) {
+            mEditTextEmail.setError("Email cannot be empty");
+            isValid = false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()) {
+            mEditTextEmail.setError("Please enter a valid email");
+            isValid = false;
+        } else {
+            mEditTextEmail.setError(null);
+        }
+
+        if (passwordInput.isEmpty()) {
+            mEditTextPassword.setError("Password cannot be empty");
+            isValid = false;
+        } else if (passwordInput.length() < 6) {
+            mEditTextEmail.setError("Password should be at least 6 characters");
+            isValid = false;
+        } else {
+            mEditTextEmail.setError(null);
+        }
+
+        if (!passwordReenterInput.equals(passwordInput)) {
+            mEditTextReenterPass.setError("Passwords do not match");
+            isValid = false;
+        } else {
+            mEditTextReenterPass.setError(null);
+        }
+
+        return isValid;
     }
 }
